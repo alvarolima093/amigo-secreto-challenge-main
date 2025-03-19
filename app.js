ac2fba071e0db819f737d3bce6598e3dd57d12da
Alvaro Lima <alvarolima093@gmail.com>
2025-03-18 23:12:49 -0300

    Adicionado sortear amigo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function adicionarAmigo() {
     console.log(listaAmigos); 
     exibirAmigos("#listaAmigos");
   } else {
-    console.error("Por favor, digite o nome de um amigo!");
+    alert("Digite o nome de um amigo!");
   }
 }
 
@@ -21,3 +21,20 @@ function exibirAmigos(tag) {
     lista.appendChild(item);
   });
 }
+function sortearAmigo() {
+    const lista = document.getElementById("listaAmigos");
+    const itens = lista.getElementsByTagName("li");
+
+    if (itens.length === 0) {
+        alert("Adicione amigos antes de sortear!");
+        return;
+    }
+
+    const indiceSorteado = Math.floor(Math.random() * itens.length);
+    const amigoSorteado = itens[indiceSorteado].textContent;
+    const resultado = document.getElementById("resultado");
+    const item = document.createElement("li");
+    item.textContent = `Amigo Sorteado: ${amigoSorteado}`;
+    resultado.appendChild(item);
+}
+
