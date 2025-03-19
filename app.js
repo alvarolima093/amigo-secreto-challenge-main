05d89bd81edaab755ff2386f6526e7a06e0f0ce8
Alvaro Lima <alvarolima093@gmail.com>
2025-03-18 22:32:23 -0300

    Criada a exibição da lista de amigos na tela

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ let listaAmigos = [];
 
 function adicionarAmigo() {
   let amigo = document.getElementById("amigo").value;
-  listaAmigos.push(amigo);
-  document.getElementById("amigo").value = "";
-  console.log(listaAmigos);
-  console.log("Amigo adicionado!");
+  if (amigo) {
+    listaAmigos.push(amigo); 
+    document.getElementById("amigo").value = ""; 
+    console.log(listaAmigos); 
+    exibirAmigos("#listaAmigos");
+  } else {
+    console.error("Por favor, digite o nome de um amigo!");
+  }
+}
+
+function exibirAmigos(tag) {
+  let lista = document.querySelector(tag);
+  lista.innerHTML = ""; 
+  listaAmigos.forEach((amigo) => {
+    let item = document.createElement("li"); 
+    item.textContent = amigo; 
+    lista.appendChild(item);
+  });
 }
